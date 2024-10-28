7f3b78b147f9c05733817ca76b49736637be95cd
Kyle Malice <Kyle.Malice2001@gmail.com>
2024-10-28 16:51:08 -0400

    ugly login pop up page, WIP!

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
 
