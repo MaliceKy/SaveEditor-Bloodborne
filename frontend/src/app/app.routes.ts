fb0d36e5cb0145c7565922720bdb479aff88ce1e
Kyle Malice <Kyle.Malice2001@gmail.com>
2024-11-12 12:58:26 -0500

    users can now navigate to dashboard without being logged in

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuardService } from './guards/auth-guard.service';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'dashboard', component: DashboardComponent },
+    { 
+        path: 'dashboard', 
+        component: DashboardComponent,
+        canActivate: [AuthGuardService]
+    },
 ];
